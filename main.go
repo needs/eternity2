b0bf55240275dac7d30f69c8f3d6f246a1dd7014
Needs <needs@mailoo.org>
2016-08-11 17:38:36 +0200

    Backtrack over every possibilities
    It does not try to rotate cells.  It does never go past 17 in
    exploration depth, so its either a bug on our side or that we cannot go
    further then 17 without rotating cells.
    
    I would vote for a bug on our side tho.

--- main.go
+++ main.go
@@ -9,30 +9,152 @@ type piece struct {
 	right int
 	bottom int
 	left int
+
+	cell *cell
 }
 
-func load_pieces() [256]piece {
-	var pieces [256]piece
+type cell struct {
+	piece *piece
+	next *cell
+	top, right, bottom, left *cell
+}
 
-	for i := 0; i < 256; i++ {
+type board struct {
+	pieces [256]piece
+	cells [16][16]cell
+}
+
+func load_pieces(pieces []piece) {
+	for i := 0; i < len(pieces); i++ {
 		fmt.Scanf(
 			"%d,%d,%d,%d,%d", &pieces[i].id,
 			&pieces[i].top, &pieces[i].right,
 			&pieces[i].bottom, &pieces[i].left)
-	}
 
-	return pieces
+		pieces[i].cell = nil
+	}
 }
 
-func print_pieces(pieces [256]piece) {
-	for i := 0; i < 256; i++ {
-		fmt.Printf(
-			"%3d: %2d %2d %2d %2d\n", pieces[i].id,
-			pieces[i].top, pieces[i].right,
-			pieces[i].bottom, pieces[i].left)
+func init_board(board *board) {
+	load_pieces(board.pieces[0:256])
+
+	// Link cell neightbors
+	for i := 0; i < len(board.cells); i++ {
+		for j := 0; j < len(board.cells[i]); j++ {
+			board.cells[i][j].piece = nil
+
+			if i == 0 {
+				board.cells[i][j].top = nil
+			} else {
+				board.cells[i][j].top = &board.cells[i - 1][j]
+			}
+
+			if i == len(board.cells) - 1 {
+				board.cells[i][j].bottom = nil
+			} else {
+				board.cells[i][j].bottom = &board.cells[i + 1][j]
+			}
+
+			if j == 0 {
+				board.cells[i][j].left = nil
+			} else {
+				board.cells[i][j].left = &board.cells[i][j - 1]
+			}
+
+			if j == len(board.cells[i]) - 1 {
+				board.cells[i][j].right = nil
+			} else {
+				board.cells[i][j].right = &board.cells[i][j + 1]
+			}
+		}
+	}
+
+	// Link cells row by row
+	for i := 0; i < len(board.cells); i++ {
+		for j := 0; j < len(board.cells[i]); j++ {
+			if j == len(board.cells[i]) - 1 && i == len(board.cells) - 1 {
+				board.cells[i][j].next = nil
+			} else if j == len(board.cells[i]) - 1 && i < len(board.cells) - 1 {
+				board.cells[i][j].next = &board.cells[i + 1][0]
+			} else {
+				board.cells[i][j].next = &board.cells[i][j + 1]
+			}
+		}
+	}
+}
+
+func does_fit(piece *piece, cell *cell) bool {
+	if cell.top != nil && cell.top.piece != nil && cell.top.piece.bottom != piece.top {
+		return false
+	} else if cell.bottom != nil && cell.bottom.piece != nil && cell.bottom.piece.top != piece.bottom {
+		return false
+	} else if cell.left != nil && cell.left.piece != nil && cell.left.piece.right != piece.left {
+		return false
+	} else if cell.right != nil && cell.right.piece != nil && cell.right.piece.left != piece.right {
+		return false
+	}
+
+	return true
+}
+
+var maxdepth = 0
+var depth = 0
+
+func backtrack(board *board, cell *cell) bool {
+	// Temporary hack to display current backtracking depth
+	if depth > maxdepth {
+		fmt.Printf("\rDepth = %d", depth)
+		maxdepth = depth
+	}
+
+	if cell == nil {
+		return true
+	} else if cell.piece != nil {
+		return backtrack(board, cell.next)
+	}
+
+	for i := 0; i < len(board.pieces); i++ {
+		if board.pieces[i].cell == nil {
+			if does_fit(&board.pieces[i], cell) {
+				board.pieces[i].cell = cell
+				cell.piece = &board.pieces[i]
+
+				depth++
+				if backtrack(board, cell.next) {
+					return true
+				}
+				depth--
+
+				board.pieces[i].cell = nil
+				cell.piece = nil
+			}
+		}
+	}
+
+	return false
+}
+
+func print_board(board *board) {
+	for i := 0; i < len(board.cells); i++ {
+		for j := 0; j < len(board.cells[i]); j++ {
+			if board.cells[i][j].piece != nil {
+				fmt.Printf("%3d ", board.cells[i][j].piece.id)
+			} else {
+				fmt.Printf("    ")
+			}
+		}
+		fmt.Printf("\n")
 	}
 }
 
 func main() {
-	print_pieces(load_pieces())
+	var board board
+
+	init_board(&board)
+
+	if backtrack(&board, &board.cells[0][0]) {
+		print_board(&board)
+	} else {
+		fmt.Printf("No solutions found\n")
+	}
 }
